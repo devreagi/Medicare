2b91818c2aa82878d5a5582aa077719f4b9c95fe
Brandon Cespedes <bc.cespedes2002@gmail.com>
2021-10-21 22:14:49 -0500

    responsive 2.0 de Cupon Valido

---
+++ app/src/main/java/co/team/blue/medicare/CuponValido.java
@@ -0,0 +1,14 @@
+package co.team.blue.medicare;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CuponValido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cupon_valido);
+    }
+}
