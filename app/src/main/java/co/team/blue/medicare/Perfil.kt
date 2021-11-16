3563bb9b69625703b19072d91b70022b3cac5557
Iván Reyes <ivan.reyesa@outlook.com>
2021-11-16 14:16:28 -0500

    Arreglar foto perifl y nombre:
    La foto y el nombre en la pantalla perfil ahora se actualizan correctamente.

--- app/src/main/java/co/team/blue/medicare/Perfil.kt
+++ app/src/main/java/co/team/blue/medicare/Perfil.kt
@@ -3,20 +3,54 @@ package co.team.blue.medicare
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.afollestad.materialdialogs.MaterialDialog
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationBarView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
 
 class Perfil : AppCompatActivity() {
-    var bottomNavigationView: BottomNavigationView? = null
+
+
+    private var bottomNavigationView: BottomNavigationView? = null
+    private var fotoPerfil: CircleImageView? = null
+    private var nombrePerfil: TextView? = null
+    private var dbReference: DatabaseReference? = null
+    private var mAuth: FirebaseAuth? = null
+    private var tag = "Ejemplo"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil)
+
+        mAuth = FirebaseAuth.getInstance()
+        dbReference = FirebaseDatabase.getInstance().reference.child("usuario")
+        val user = mAuth!!.currentUser
+
         bottomNavigationView = findViewById(R.id.footer)
+        fotoPerfil = findViewById(R.id.imgFotoPerfil)
+        nombrePerfil = findViewById(R.id.txtNombreUsuario)
+
+        //carga inforusuario
+        if (user != null) {
+            infoUsuario()
+        } else {
+            mAuth!!.signInAnonymously().addOnSuccessListener(this) {
+                Log.e(tag, "Inicia sesion como anonimo/invitado")
+                infoUsuario()
+            }.addOnFailureListener(this) { exception: Exception? ->
+                Log.e(tag, "signInAnonymously:FAILURE", exception)
+            }
+        }
+
         bottomNavigationView?.run {
             isSelected = true
             setOnItemSelectedListener(NavigationBarView.OnItemSelectedListener setOnItemSelectedListener@{ item: MenuItem ->
@@ -42,6 +76,26 @@ class Perfil : AppCompatActivity() {
         }
     }
 
+    private fun infoUsuario() {
+        mAuth!!.currentUser?.let {
+            dbReference!!.child(it.uid)
+                .addValueEventListener(object : ValueEventListener {
+                    override fun onDataChange(dataSnapshot: DataSnapshot) {
+                        if (dataSnapshot.exists() && dataSnapshot.childrenCount > 0) {
+                            val nombre = (dataSnapshot.child("nombre").value).toString()
+                            nombrePerfil?.text = nombre
+                            if (dataSnapshot.hasChild("imagen")) {
+                                val image = (dataSnapshot.child("imagen").value).toString()
+                                Picasso.get().load(image).into(fotoPerfil)
+                            }
+                        }
+                    }
+
+                    override fun onCancelled(databaseError: DatabaseError) {}
+                })
+        }
+    }
+
     //ir  a editar campos del perfil
     fun irEditarPerfil(@Suppress("UNUSED_PARAMETER") view: View?) {
         val editarPerfil = Intent(this, EditarPerfil::class.java)
@@ -55,7 +109,7 @@ class Perfil : AppCompatActivity() {
             icon(R.drawable.power_off)
             title(R.string.cerrar_sesi_n)
             message(R.string.msg_cerrar_sesion)
-            positiveButton(R.string.si) { dialog ->
+            positiveButton(R.string.si) {
                 startActivity(home)
             }
             negativeButton(R.string.no) { dialog ->
