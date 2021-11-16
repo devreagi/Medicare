2265ba0e1ac469e4c0217d17e92bbf50447dca20
Iván Reyes <ivan.reyesa@outlook.com>
2021-11-15 20:11:59 -0500

    Lógica editar perfil.

--- app/src/main/java/co/team/blue/medicare/EditarPerfil.java
+++ app/src/main/java/co/team/blue/medicare/EditarPerfil.java
@@ -1,18 +1,94 @@
 package co.team.blue.medicare;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+
+import java.util.HashMap;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class EditarPerfil extends AppCompatActivity {
+
+    private CircleImageView profileImageView;
+    private Button closeButton, saveButton;
+    private ImageView profileChangeBtn;
+    private EditText edtName, edtPhone;
+
+    private DatabaseReference databaseReference;
+    private FirebaseAuth mAuth;
+
+    private Uri imageUri;
+    private String myUri = "";
+    private StorageTask uploadTask;
+    private StorageReference storageProfilePicsRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editar_perfil);
+
+        mAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("User");
+        storageProfilePicsRef = FirebaseStorage.getInstance().getReference().child("Profile Pic");
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        profileImageView = findViewById(R.id.profile_image);
+
+        closeButton = findViewById(R.id.btnClose);
+        saveButton = findViewById(R.id.btnSave);
+        edtName = findViewById(R.id.name);
+        edtPhone = findViewById(R.id.phone_number);
+
+        profileChangeBtn = findViewById(R.id.change_profile_btn);
+
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(EditarPerfil.this, Perfil.class));
+            }
+        });
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validateAndsave();
+            }
+        });
+
+        profileChangeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CropImage.activity().setAspectRatio(1, 1).start(EditarPerfil.this);
+            }
+        });
+
+        getUserinfo();
     }
 
     //info ubicacion
@@ -21,20 +97,109 @@ public class EditarPerfil extends AppCompatActivity {
         startActivity(direccion);
     }
 
-    //cerrar edicion
-    public void irPerfilUsuario(View view) {
-        Intent perfil = new Intent(this, Perfil.class);
-        startActivity(perfil);
+    private void validateAndsave() {
+        if (TextUtils.isEmpty(edtName.getText().toString())) {
+            Toast.makeText(this, "Please Enter Your Name", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(edtPhone.getText().toString())) {
+            Toast.makeText(this, "Please Enter Your Phone No. ", Toast.LENGTH_SHORT).show();
+        } else {
+            HashMap<String, Object> userMap = new HashMap<>();
+            userMap.put("name", edtName.getText().toString());
+            userMap.put("phone", edtPhone.getText().toString());
+
+            databaseReference.child(mAuth.getCurrentUser().getUid()).updateChildren(userMap);
+
+            uploadProfileImage();
+        }
     }
 
-    //Guardar/ actualzar datos
-    public void guardarDatos(View view) {
-        boolean guardadoExitoso = false;
-        if (guardadoExitoso) {
-            Intent perfil = new Intent(this, Perfil.class);
-            startActivity(perfil);
+
+    private void getUserinfo() {
+        databaseReference.child(mAuth.getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists() && dataSnapshot.getChildrenCount() > 0) {
+                    String name = dataSnapshot.child("name").getValue().toString();
+                    String phone = dataSnapshot.child("phone").getValue().toString();
+
+                    edtName.setText(name);
+                    edtPhone.setText(phone);
+
+                    if (dataSnapshot.hasChild("image")) {
+                        String image = dataSnapshot.child("image").getValue().toString();
+                        Picasso.get().load(image).into(profileImageView);
+                    }
+                }
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            imageUri = result.getUri();
+
+            profileImageView.setImageURI(imageUri);
         } else {
-            Toast.makeText(this, "HUBO UN ERROR GUARDANDO LA DIRECCION ", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Error, Try again", Toast.LENGTH_SHORT).show();
         }
+
+    }
+
+    private void uploadProfileImage() {
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Set your profile");
+        progressDialog.setMessage("Please wait, while we are setting your data ");
+        progressDialog.show();
+
+        if (imageUri != null) {
+            final StorageReference fileRef = storageProfilePicsRef
+                    .child(mAuth.getCurrentUser().getUid() + ".jpg");
+
+            uploadTask = fileRef.putFile(imageUri);
+
+
+            uploadTask.continueWithTask(new Continuation() {
+                @Override
+                public Object then(@NonNull Task task) throws Exception {
+                    if (!task.isSuccessful()) {
+                        throw task.getException();
+                    }
+                    return fileRef.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()) {
+                        Uri downloadUrl = task.getResult();
+                        myUri = downloadUrl.toString();
+
+                        HashMap<String, Object> userMap = new HashMap<>();
+                        userMap.put("image", myUri);
+
+                        databaseReference.child(mAuth.getCurrentUser().getUid()).updateChildren(userMap);
+
+                        progressDialog.dismiss();
+
+
+                    }
+
+                }
+            });
+        } else {
+            progressDialog.dismiss();
+            Toast.makeText(this, "Data update", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
--- app/src/main/java/co/team/blue/medicare/MainActivity.java
+++ app/src/main/java/co/team/blue/medicare/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detalle_pedido);
+        setContentView(R.layout.activity_main);
     }
 
     //metodo boton ir a perfil de usuario
