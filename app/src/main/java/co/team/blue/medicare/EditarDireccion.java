f3e454dda5c672d894ab2fec6b7d95725372011b
Iván Reyes <ivan.reyesa@outlook.com>
2021-11-19 11:46:30 -0500

    Editar direccion correctamente.

--- app/src/main/java/co/team/blue/medicare/EditarDireccion.java
+++ app/src/main/java/co/team/blue/medicare/EditarDireccion.java
@@ -5,11 +5,16 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,10 +22,13 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Objects;
 
+import co.team.blue.medicare.data.Ciudad;
 import co.team.blue.medicare.data.Departamento;
+import co.team.blue.medicare.data.Nomenclatura;
 import co.team.blue.medicare.data.Pais;
 
 public class EditarDireccion extends AppCompatActivity {
@@ -30,8 +38,13 @@ public class EditarDireccion extends AppCompatActivity {
     Spinner spinnerCiudad;
     Spinner spinnerDireccion;
     DatabaseReference mDatabase;
+    String paisSeleccionado = "";
     String departamentoSeleccionado = "";
     String ciudadSeleccionada = "";
+    String direccionSeleccionada = "";
+    EditText txtOpcionDir;
+    EditText txtNumDir;
+    Button btnGuardar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,9 +55,19 @@ public class EditarDireccion extends AppCompatActivity {
         spinnerDepartamento = findViewById(R.id.spinnerDepartamento);
         spinnerCiudad = findViewById(R.id.spinnerCiudad);
         spinnerDireccion = findViewById(R.id.spinnerDireccion);
-        mDatabase = FirebaseDatabase.getInstance().getReference();
+        txtOpcionDir = findViewById(R.id.txtOpcionDir);
+        txtNumDir = findViewById(R.id.txtNumDir);
+        btnGuardar = findViewById(R.id.btnGuardarDireccion);
 
-        cargarPais();
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        FirebaseUser user = mAuth.getCurrentUser();
+
+        if (user != null) {
+            cargarPais();
+            cargarNomenclaturas();
+            btnGuardar.setOnClickListener(v -> guardarDireccion(user));
+        }
     }
 
     //carga paises en spinner
@@ -59,12 +82,13 @@ public class EditarDireccion extends AppCompatActivity {
                         String id = ds.getKey();
                         paises.add(new Pais(id, nombre));
                     }
-
                     ArrayAdapter<Pais> arrayAdapter = new ArrayAdapter<>(EditarDireccion.this, android.R.layout.simple_dropdown_item_1line, paises);
                     spinnerPaises.setAdapter(arrayAdapter);
+
                     spinnerPaises.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                         @Override
                         public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            paisSeleccionado = parent.getItemAtPosition(position).toString();
                             cargarDepartamentos(String.valueOf(position));
                         }
 
@@ -102,6 +126,7 @@ public class EditarDireccion extends AppCompatActivity {
                     spinnerDepartamento.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                         @Override
                         public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            departamentoSeleccionado = parent.getItemAtPosition(position).toString();
                             cargarCiudades(idPais, String.valueOf(position));
                         }
 
@@ -122,7 +147,7 @@ public class EditarDireccion extends AppCompatActivity {
 
     //carga ciudades en spinner
     public void cargarCiudades(String idPais, String idDepartamento) {
-        final List<Departamento> ciudades = new ArrayList<>();
+        final List<Ciudad> ciudades = new ArrayList<>();
         mDatabase.child("pais").child(idPais).child("states").child(idDepartamento).child("cities").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -130,16 +155,15 @@ public class EditarDireccion extends AppCompatActivity {
                     for (DataSnapshot ds : snapshot.getChildren()) {
                         String nombre = Objects.requireNonNull(ds.child("name").getValue()).toString();
                         String id = ds.getKey();
-                        ciudades.add(new Departamento(id, nombre));
+                        ciudades.add(new Ciudad(id, nombre));
                     }
 
-                    ArrayAdapter<Departamento> arrayAdapter = new ArrayAdapter<>(EditarDireccion.this, android.R.layout.simple_dropdown_item_1line, ciudades);
+                    ArrayAdapter<Ciudad> arrayAdapter = new ArrayAdapter<>(EditarDireccion.this, android.R.layout.simple_dropdown_item_1line, ciudades);
                     spinnerCiudad.setAdapter(arrayAdapter);
                     spinnerCiudad.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
                         @Override
                         public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                             ciudadSeleccionada = parent.getItemAtPosition(position).toString();
-
                         }
 
                         @Override
@@ -158,12 +182,61 @@ public class EditarDireccion extends AppCompatActivity {
     }
 
 
+    // carga nomenclaturas en spinner
+
+
+    public void cargarNomenclaturas() {
+        final List<Nomenclatura> nomenclaturas = new ArrayList<>();
+        mDatabase.child("nomenclatura").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()) {
+                    for (DataSnapshot ds : snapshot.getChildren()) {
+                        String nombre = Objects.requireNonNull(ds.child("name").getValue()).toString();
+                        String id = ds.getKey();
+                        nomenclaturas.add(new Nomenclatura(id, nombre));
+                    }
+
+                    ArrayAdapter<Nomenclatura> arrayAdapter = new ArrayAdapter<>(EditarDireccion.this, android.R.layout.simple_dropdown_item_1line, nomenclaturas);
+                    spinnerDireccion.setAdapter(arrayAdapter);
+                    spinnerDireccion.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            direccionSeleccionada = parent.getItemAtPosition(position).toString();
+                            cargarDepartamentos(String.valueOf(position));
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
     //Guardar/ actualzar datos
-    public void guardarDatos(View view) {
-        boolean guardadoExitoso = false;
-        if (guardadoExitoso) {
-            Intent editarPerfil = new Intent(this, EditarPerfil.class);
-            startActivity(editarPerfil);
-        }
+    private void guardarDireccion(FirebaseUser user) {
+        HashMap<String, Object> userMap = new HashMap<>();
+        userMap.put("pais", paisSeleccionado);
+        userMap.put("departamento", departamentoSeleccionado);
+        userMap.put("ciudad", ciudadSeleccionada);
+        userMap.put("direccion", direccionSeleccionada);
+        userMap.put("dirOpcion", txtOpcionDir.getText().toString());
+        userMap.put("dirNum", txtNumDir.getText().toString());
+
+        mDatabase.child("usuario").child(user.getUid()).child("ubicacion").updateChildren(userMap);
+
+        Toast.makeText(this, "Direccion actualizada", Toast.LENGTH_SHORT).show();
+
+        Intent editarPerfil = new Intent(this, EditarPerfil.class);
+        startActivity(editarPerfil);
     }
 }
